22e5ce2387e4fcaa017b7a6f3659b9810c5564c3
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-26 04:41:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    text.scan(/[\AA-Z]\w+\!/)
+    text.(/[\AA-Z]\w+\!/)
 end
 
 def valid_phone_number?(phone)
